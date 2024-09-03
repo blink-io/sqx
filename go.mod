eab93820988192975c44082dcc9235f046bd8365
heisonyee <heisonyee@126.com>
2024-09-03 13:19:01 +0800

    dd

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/blink-io/sqx
+
+go 1.23.0
+
+require github.com/bokwoon95/sq v0.5.1
---
+++ sqx.go
@@ -0,0 +1,12 @@
+package sqx
+
+import (
+	"github.com/bokwoon95/sq"
+)
+
+const (
+	DialectSQLite    = sq.DialectSQLite
+	DialectPostgres  = sq.DialectPostgres
+	DialectMySQL     = sq.DialectMySQL
+	DialectSQLServer = sq.DialectSQLServer
+)
